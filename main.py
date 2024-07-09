48e0776ee0be61599f863a714f8c57ab1c41bde6
Kayla <kayla.marie.792@gmail.com>
2024-07-09 14:15:54 -0400

    end of video 2, player can jump and move and will not fall off screen

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ import sys
 
 pygame.init()
 
+screen_width = 1000
+screen_height = 1000
+
+tile_size = 50
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Platformer")
+
 ########## FUNCTIONS ###########
 
 class World():
     def __init__(self, data):
         self.tile_list = []
-
 
         # load images
         dirt_img = pygame.image.load('img/dirt.png')
@@ -42,7 +49,55 @@ class World():
     def draw(self):
         for tile in self.tile_list:
             screen.blit(tile[0], tile[1])
-    
+
+class Player():
+    def __init__(self, x, y):
+        img = pygame.image.load('img/guy1.png')
+        self.image = pygame.transform.scale(img, (40, 80))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.vel_y = 0
+        self.jumped = False
+
+    def update(self):
+
+        dx = 0
+        dy = 0 
+
+        # get key presses
+        key = pygame.key.get_pressed()
+        if key[pygame.K_SPACE] and self.jumped == False:
+            self.vel_y = -15
+            self.jumped = True
+            pass
+        if key[pygame.K_SPACE] == False:
+            self.jumped = False
+        if key[pygame.K_LEFT]:
+            dx -= 5
+        if key[pygame.K_RIGHT]:
+            dx += 5
+
+        # Add our Gravity
+
+        self.vel_y += 1
+        # Add a maximum velocity
+        if self.vel_y > 10:
+            self.vel_y = 10
+        dy += self.vel_y
+
+        # check for collisions
+
+        # update player coordinates
+        self.rect.x += dx
+        self.rect.y += dy
+
+        if self.rect.bottom > screen_height:
+            self.rect.bottom = screen_height
+
+
+        # draw our player
+        screen.blit(self.image, self.rect)
 
 def draw_grid():
     for line in range(0, int(screen_width/tile_size + 1)):
@@ -51,24 +106,35 @@ def draw_grid():
 
 ########## GAME LOGIC ##########
 
-screen_width = 1000
-screen_height = 1000
 
-tile_size = 50
 
 world_data = [
-    [1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [2, 2, 2, 2, 2],
+[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 1], 
+[1, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 7, 0, 5, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 1], 
+[1, 7, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 7, 0, 0, 0, 0, 1], 
+[1, 0, 2, 0, 0, 7, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 2, 0, 0, 4, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 7, 0, 0, 0, 0, 2, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], 
+[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2, 0, 2, 2, 2, 2, 2, 1], 
+[1, 0, 0, 0, 0, 0, 2, 2, 2, 6, 6, 6, 6, 6, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 
+[1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
 
-world = World(world_data)
-print(world.tile_list)
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Platformer")
+world = World(world_data)
+player = Player(100, screen_height - 130)
+
 
 # Load our images
 sun_img = pygame.image.load('img/sun.png')
@@ -76,18 +142,21 @@ bg_img = pygame.image.load('img/sky.png')
 
 run = True
 
-while True:
-
-    for event in pygame.event.get():
-        if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
-            exit()
+while run:
 
     screen.blit(bg_img, (0 ,0))
     screen.blit(sun_img, (100, 100))
 
     world.draw()
 
-    draw_grid()
+    player.update()
+
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+            run = False
+
+
+    # draw_grid()
 
     pygame.display.update()
 
