1de66a316d8da82843d29ef704009029ef345b08
Jared De Blander <jared0x90@gmail.com>
2024-07-25 00:01:01 -0400

    fixed imports

--- Game.py
+++ Game.py
@@ -7,22 +7,24 @@ import pickle
 
 from config import *
 
-from Player import Player
+from player import Player
 from world import World
-from Coin import Coin
-from Button import Button
+from coin import Coin
+from button import Button
 
 
-class Game():
+class Game:
     def __init__(self):
-                
+
         pygame.mixer.pre_init(44100, -16, 2, 512)
         pygame.init()
         mixer.init()
         self.fps = 60
         self.clock = pygame.time.Clock()
 
-        self.screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN | pygame.SCALED)
+        self.screen = pygame.display.set_mode(
+            (screen_width, screen_height), pygame.FULLSCREEN | pygame.SCALED
+        )
         pygame.display.set_caption("Platformer")
 
         self.game_over = 0
@@ -31,16 +33,16 @@ class Game():
         self.score = 0
 
         # Load our images
-        self.sun_img = pygame.image.load('assets/img/sun.png')
-        self.bg_img = pygame.image.load('assets/img/sky.png')
-        self.restart_img = pygame.image.load('assets/img/restart_btn.png')
-        self.start_img = pygame.image.load('assets/img/start_btn.png')
-        self.exit_img = pygame.image.load('assets/img/exit_btn.png')
+        self.sun_img = pygame.image.load("assets/img/sun.png")
+        self.bg_img = pygame.image.load("assets/img/sky.png")
+        self.restart_img = pygame.image.load("assets/img/restart_btn.png")
+        self.start_img = pygame.image.load("assets/img/start_btn.png")
+        self.exit_img = pygame.image.load("assets/img/exit_btn.png")
 
         # Load our sounds
-        pygame.mixer.music.load('assets/audio/music.wav')
+        pygame.mixer.music.load("assets/audio/music.wav")
         pygame.mixer.music.play(-1, 0.0, 5000)
-        coin_fx = pygame.mixer.Sound('assets/audio/coin.wav')
+        coin_fx = pygame.mixer.Sound("assets/audio/coin.wav")
         coin_fx.set_volume(0.5)
 
         self.world = self.reset_level(self.level)
@@ -52,26 +54,35 @@ class Game():
         self.coin_group.add(Coin(tile_size // 2, tile_size // 2 + 3))
 
         # buttons
-        self.restart_button = Button(self.screen, screen_width // 2 -50, screen_height // 2 + 100, self.restart_img)
-        self.start_button = Button(self.screen, screen_width // 2 - 350, screen_height // 2, self.start_img)
-        self.exit_button = Button(self.screen, screen_width // 2 + 150, screen_height // 2, self.exit_img)
+        self.restart_button = Button(
+            self.screen,
+            screen_width // 2 - 50,
+            screen_height // 2 + 100,
+            self.restart_img,
+        )
+        self.start_button = Button(
+            self.screen, screen_width // 2 - 350, screen_height // 2, self.start_img
+        )
+        self.exit_button = Button(
+            self.screen, screen_width // 2 + 150, screen_height // 2, self.exit_img
+        )
 
-    def reset_level(self, level) -> World:            
-        if path.exists(f'level_data/level{level}_data'):
-            pickle_in = open(f'level_data/level{level}_data', 'rb')
-            print(f'Loading level {level}')
+    def reset_level(self, level) -> World:
+        if path.exists(f"level_data/level{level}_data"):
+            pickle_in = open(f"level_data/level{level}_data", "rb")
+            print(f"Loading level {level}")
         else:
-            pickle_in = open(f'level_data/level0_data', 'rb')
-            print(f'Failed to find data file for level {level}. Loading level 0.')
+            pickle_in = open(f"level_data/level0_data", "rb")
+            print(f"Failed to find data file for level {level}. Loading level 0.")
         world_data = pickle.load(pickle_in)
         world = World(self.screen, world_data)
 
         return world
-        
+
     def update(self):
         self.clock.tick(self.fps)
 
-        self.screen.blit(self.bg_img, (0 ,0))
+        self.screen.blit(self.bg_img, (0, 0))
         self.screen.blit(self.sun_img, (100, 100))
 
         if main_menu:
@@ -88,8 +99,8 @@ class Game():
                 if pygame.sprite.spritecollide(self.player, self.coin_group, True):
                     self.coin_fx.play()
                     score += 1
-                    print(f'Score: {score}')
-                self.draw_text('X ' + str(score), font_score, white, tile_size - 10, 10)
+                    print(f"Score: {score}")
+                self.draw_text("X " + str(score), font_score, white, tile_size - 10, 10)
                 self.coin_group.draw(self.screen)
                 self.world.blob_group.update()
                 self.world.blob_group.draw(self.screen)
@@ -100,7 +111,13 @@ class Game():
 
             # Player has died
             if self.game_over == -1:
-                self.draw_text('Game Over!', font, white, screen_width // 2 - 275, screen_height // 2 - 100)
+                self.draw_text(
+                    "Game Over!",
+                    font,
+                    white,
+                    screen_width // 2 - 275,
+                    screen_height // 2 - 100,
+                )
                 if self.restart_button.draw():
                     self.world = self.reset_level(level)
                     self.player.reset(100, screen_height - 130)
@@ -109,14 +126,26 @@ class Game():
             # Player has won!
             if self.game_over == 1:
                 if self.level >= max_level:
-                    self.draw_text('You Win!', font, white, screen_width // 2 - 240, screen_height // 2 - 175)
-                    self.draw_text(f'Score: {score}', font, white, screen_width // 2 - 215, screen_height // 2 - 50)
+                    self.draw_text(
+                        "You Win!",
+                        font,
+                        white,
+                        screen_width // 2 - 240,
+                        screen_height // 2 - 175,
+                    )
+                    self.draw_text(
+                        f"Score: {score}",
+                        font,
+                        white,
+                        screen_width // 2 - 215,
+                        screen_height // 2 - 50,
+                    )
                     if self.restart_button.draw():
                         self.level = 0
                         self.score = 0
                         self.world = self.reset_level(self.level)
                         self.game_over = 0
-                else: 
+                else:
                     self.level += 1
                     self.world = self.reset_level(self.level)
                     self.game_over = 0
